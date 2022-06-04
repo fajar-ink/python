ca315755b999f0c199a38a32ebe733322a9308aa
Fajar Sugeng Ariadi <57746316+fajar-ink@users.noreply.github.com>
2022-06-04 20:04:28 +0700

    Add files via upload

---
+++ Program Alamat Lengkap (Indo).py
@@ -0,0 +1,159 @@
+# DATA NEGARA
+
+# DATA PROVINSI
+daftar_provinsi = ['Prov Bali', 'Prov Bangka Belitung', 'Prov Prov Banten', 'Prov Bengkulu', 'Prov DI Yogyakarta', 'Prov DKI Jakarta', 'Prov Gorontalo', 'Prov Jambi', 'Prov Jawa Barat', 'Prov Jawa Tengah', 'Prov Jawa Timur', 'Prov Kalimantan Barat', 'Prov Kalimantan Selatan', 'Prov Kalimantan Tengah', 'Prov Kalimantan Timur', 'Prov Kalimantan Utara',
+                   'Prov Kepulauan Riau', 'Prov Lampung', 'Prov Maluku', 'Prov Maluku Utara', 'Prov Nangroe Aceh Darussalam', 'Prov Nusa Tenggara Barat', 'Prov Nusa Tenggara Timur', 'Prov Papua', 'Prov Papua Barat', 'Prov Riau', 'Prov Sulawesi Barat', 'Prov Sulawesi Selatan', 'Prov Sulawesi Tengah', 'Prov Sulawesi Tenggara', 'Prov Sulawesi Utara', 'Prov Sumatera Barat', 'Prov Sumatera Selatan', 'Prov Sumatera Utara']
+
+
+# DATA KABUPATEN ATAU KOTA
+daftar_kabupaten = [['Kab Badung', 'Kab Bangli', 'Kab Buleleng', 'Kab Gianyar', 'Kab Jembrana',
+                     'Kab Karangasem', 'Kab Klungkung', 'Kab Tabanan', 'Kota Denpasar'], ['Kab Bangka', 'Kab Bangka Barat', 'Kab Bangka Selatan',
+                                                                                          'Kab Bangka Tengah', 'Kab Belitung', 'Kab Belitung Timur', 'Kota Pangkal Pinang'], ['Kab Lebak', 'Kab Pandeglang', 'Kab Serang', 'Kab Tangerang',
+                                                                                                                                                                              'Kota Cilegon', 'Kota Serang', 'Kota Tangerang', 'Kota Tangerang Selatan'], ['Kab Bengkulu Selatan', 'Kab Bengkulu Tengah', 'Kab Bengkulu Utara', 'Kab Kaur',
+                                                                                                                                                                                                                                                           'Kab Kapahiang', 'Kab Lebing', 'Kab Mukomuko', 'Kab Rejang Lebong', 'Kab Seluma', 'Kota Bengkulu'], ['Kab Bantul', 'Kab Gunungkidul',
+                                                                                                                                                                                                                                                                                                                                                                'Kab Kulon Progo', 'Kab Sleman', 'Kota Yogyakarta'], ['Kota Administrasi Jakarta Barat', 'Kota Administrasi Jakarta Pusat', 'Kota Administarasi Jakarta Selatan',
+                                                                                                                                                                                                                                                                                                                                                                                                                      'Kota Administrasi Jakarta Timur', 'Kota Administrasi Jakarta Utara', 'Kota Administrasi Kepulauan Seribu'], ['Kab Boalemo', 'Kab Bone Bolango', 'Kab Gorontalo',
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    'Kab Gorontalo Utara', 'Kab Pahuwato', 'Kota Gorontalo'], ['Kab Batanghari', 'Kab Bungo', 'Kab Kerinci', 'Kab Merangin', 'Kab Muaro Jambi', 'Kab Sarolangun',
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               'Kab Tanjung Jabung Barat', 'Kab Tanjung Jabung Timur', 'Kab Tebo', 'Kota Jambi', 'Kota Sungai Penuh'], ['Kab Bandung', 'Kab Bandung Barat', 'Kab Bekasi', 'Kab Bogor', 'Kab Ciamis', 'Kab Cianjur', 'Kab Cirebon', 'Kab Garut', 'Kab Indramayu', 'Kab Karawang', 'Kab Kuningan', 'Kab Majalengka', 'Kab Pangandaran',
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        'Kab Purwakarta', 'Kab Subang', 'Kab Sukabumi', 'Kab Sumedang', 'Kab Tasikmalaya', 'Kota Bandung', 'Kota Banjar', 'Kota Bekasi', 'Kota Bogor', 'Kota Cimahi', 'Kota Cirebon', 'Kota Depok', 'Kota Sukabumi', 'Kota Tasikmalaya'], ['Kab Banjarnegara', 'Kab Banyumas', 'Kab Batang', 'Kab Blora', 'Kab Boyolali', 'Kab Brebes', 'Kab Cilacap', 'Kab Demak', 'Kab Purwodadi', 'Kab Jepara', 'Kab Karanganyar', 'Kab Kebumen', 'Kab Kendal', 'Kab Klaten', 'Kab Kudus', 'Kab Magelang', 'Kab Pati', 'Kab Pekalongan', 'Kab Pemalang',
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           'Kab Purbalingga', 'Kab Purworejo', 'Kab Rembang', 'Kab Semarang', 'Kab Sragen', 'Kab Sukoharjo', 'Kab Tegal', 'Kab Temanggung', 'Kab Wonogiri', 'Kab Wonosobo', 'Kota Magelang', 'Kota Pekalongan', 'Kota Salatiga', 'Kota Semarang', 'Kota Surakarta', 'Kota Tegal'], ['Kab Bangkalan', 'Kab Banyuwangi', 'Kab Blitar', 'Kab Bojonegoro', 'Kab Bondowoso', 'Kab Gresik', 'Kab Jember', 'Kab Jombang', 'Kab Kediri', 'Kab Lamongan', 'Kab Lumajang', 'Kab Madiun', 'Kab Magetan', 'Kab Malang', 'Kab Mojokerto', 'Kab Nganjuk', 'Kab Ngawi', 'Kab Pacitan', 'Kab Pamekasan',
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    'Kab Pasuruan', 'Kab Ponorogo', 'Kab Probolinggo', 'Kab Sampang', 'Kab Sidoarjo', 'Kab Situbondo', 'Kab Sumenep', 'Kab Trenggalek', 'Kab Tuban', 'Kab Tulungagung', 'Kota Batu', 'Kota Blitar', 'Kota Kediri', 'Kota Madiun', 'Kota Malang', 'Kota Mojokerto', 'Kota Pasuruan', 'Kota Probolinggo', 'Kota Surabaya']]
+
+'''
+bali = daftar_kabupaten[0]
+babel = daftar_kabupaten[1]
+banten = daftar_kabupaten[2]
+bengkulu = daftar_kabupaten[3]
+diy = daftar_kabupaten[4]
+dki = daftar_kabupaten[5]
+gorontalo = daftar_kabupaten[6]
+jambi = daftar_kabupaten[7]
+jabar = daftar_kabupaten[8]
+jateng = daftar_kabupaten[9]
+jatim = daftar_kabupaten[10]
+kalbar = daftar_kabupaten[11]
+kalsel = daftar_kabupaten[12]
+kalteng = daftar_kabupaten[13]
+kaltim = daftar_kabupaten[14]
+kaltara = daftar_kabupaten[15]
+kepri = daftar_kabupaten[16]
+lampung = daftar_kabupaten[17]
+maluku = daftar_kabupaten[18]
+malut = daftar_kabupaten[19]
+nad = daftar_kabupaten[20]
+ntb = daftar_kabupaten[21]
+ntt = daftar_kabupaten[22]
+papua = daftar_kabupaten[23]
+pabar = daftar_kabupaten[24]
+riau = daftar_kabupaten[25]
+sulbar = daftar_kabupaten[26]
+sulsel = daftar_kabupaten[27]
+sulteng = daftar_kabupaten[28]
+sultra = daftar_kabupaten[29]
+sulut = daftar_kabupaten[30]
+sumbar = daftar_kabupaten[31]
+sumsel = daftar_kabupaten[32]
+sumut = daftar_kabupaten[33]
+'''
+
+
+# DATA KECAMATAN
+
+daftar_kecamatan = [[['Kec Abiansemal', 'Kec Kuta', 'Kec Kuta Selatan', 'Kec Kuta Utara', 'Kec Mengwi', 'Kec Petang'], ['Kec Bangli', 'Kec Kintamani', 'Kec Susut', 'Kec Tembuku'], ['Kec Banjar', 'Kec Buleleng', 'Kec Busung Biu', 'Kec Gerokgak', 'Kec Kubutambahan',
+                                                                                                                                                                                     'Kec Sawan', 'Kec Seririt', 'Kec Sukasada', 'Kec Tejakula'], ['Kec Blahbatuh', 'Kec Gianyar', 'Kec Payangan', 'Kec Sukawati', 'Kec Tampaksiring', 'Kec Tegallalang', 'Kec Ubud'], ['Kec Jembrana', 'Kec Melaya', 'Kec Mendoyo', 'Kec Negara', 'Kec Pekutatan'], ['Kec Abang', 'Kec Babandem', 'Kec Kubu', 'Kec Manggis', 'Kec Rendang', 'Kec Selat', 'Kec Sidemen'], ['Kec Banjarangkan', 'Kec Dawan', 'Kec Klungkung', 'Kec Nusa Penida'], ['Kec Baturiti', 'Kec Kediri', 'Kec Kerambitan', 'Kec Marga', 'Kec Penebel', 'Kec Pupuan', 'Kec Selemadeg', 'Kec Selemadeg Barat', 'Kec Selemadeg Timur', 'Kec Tabanan'], ['Kec Denpasar Barat', 'Kec Denpasar Selatan', 'Kec Denpasar Timur', 'Kec Denpasar Utara']], [['Kec Bakam', 'Kec Belinyu', 'Kec Mendo Barat', 'Kec Merawang', 'Kec Pemali', 'Kec Puding Besar', 'Kec Riau Silip', 'Kec Sungai Liat'], ['Kec Jebus', 'Kec Kelapa', 'Kec Muntok', 'Kec Parittiga', 'Kec Simpang Teritip', 'Kec Tempilang'], ['Kec Air Gegas', 'Kec Kepulauan Pongok', 'Kec Lepar Pongok', 'Kec Payung', 'Kec Pulaubesar', 'Kec Simpang Rimba', 'Kec Toboali', 'Kec Tukak Sadai'], ['Kec Koba', 'Kec Lubuk Besar', 'Kec Namang', 'Kec Pangkalan Baru', 'Kec Simpang Katis', 'Kec Sungai Selan'], ['Kec Badau', 'Kec Membalong', 'Kec Selat Nasik', 'Kec Sijuk', 'Kec Tanjung Pandan'], ['Kec Damar', 'Kec Dendang', 'Kec Gantung', 'Kec Kelapa Kampit', 'Kec Manggar', 'Kec Simpang Pesak', 'Kec Simpang Renggiang'], ['Kec Bukit Intan', 'Kec Gabek', 'Kec Gerunggang', 'Kec Girimaya', 'Kec Pangkal Balam', 'Kec Rangkui', 'Kec Taman Sari']]]
+'''
+Badung = daftar_kecamatan[0][1]
+Bangli = daftar_kecamatan[0][2]
+Buleleng = daftar_kecamatan[0][3]
+Gianyar = daftar_kecamatan[0][4]
+Jembrana = daftar_kecamatan[0][5]
+Karangasem = daftar_kecamatan[0][6]
+Klungkung = daftar_kecamatan[0][7]
+Tabanan = daftar_kecamatan[0][8]
+Denpasar = daftar_kecamatan[0][9]
+'''
+
+
+# DATA DESA
+
+
+# DATA DUKUH
+
+
+#
+#
+#
+#
+#
+#
+
+print("----------------------")
+print("MASUKKAN NAMA LENGKAP ANDA")
+nama = input("Masukkan nama : ")
+
+
+def input_provinsi():
+    print("----------------------")
+    print("PILIH PROVINSI ANDA")
+    for i, prov in enumerate(daftar_provinsi):
+        print(i+1, prov)
+    global pilih_prov
+    pilih_prov = int(input("Pilih nomor : "))
+    print("")
+    if pilih_prov >= 1 and pilih_prov <= 34:
+        print("Provinsi anda adalah", daftar_provinsi[pilih_prov-1])
+        print("")
+    else:
+        print("[!] Pilihan Anda salah, ulangi!")
+        input_provinsi()
+
+
+def input_kabupaten():
+    print("----------------------")
+    print("PILIH KABUPATEN ATAU KOTA ANDA")
+    for i, kabupaten in enumerate(daftar_kabupaten[pilih_prov-1]):
+        print(i+1, kabupaten)
+    global pilih_kabupaten
+    pilih_kabupaten = int(input("Pilih nomor : "))
+    if pilih_kabupaten >= 1 and pilih_kabupaten <= len(daftar_kabupaten[pilih_prov-1]):
+        global kabupaten_anda
+        kabupaten_anda = daftar_kabupaten[pilih_prov-1]
+        print("")
+        print("Kabupaten / Kota anda adalah",
+              kabupaten_anda[pilih_kabupaten-1])
+        print("")
+    else:
+        print("[!] Pilihan salah, ulangi!\n")
+        input_kabupaten()
+
+
+def input_kecamatan():
+    print("----------------------")
+    print("PILIH KECAMATAN ANDA")
+    for i, kecamatan in enumerate(daftar_kecamatan[pilih_prov-1][pilih_kabupaten-1]):
+        print(i+1, kecamatan)
+    global pilih_kecamatan
+    pilih_kecamatan = int(input("Pilih nomor : "))
+    if pilih_kecamatan >= 1 and pilih_kecamatan <= len(daftar_kecamatan[pilih_prov-1][pilih_kabupaten-1]):
+        global kecamatan_anda
+        kecamatan_anda = daftar_kecamatan[pilih_prov-1][pilih_kabupaten-1]
+        print("")
+        print("Kecamatan anda adalah",
+              kecamatan_anda[pilih_kecamatan-1])
+        print("")
+    else:
+        print("[!] Pilihan salah, ulangi!\n")
+        input_kecamatan()
+
+
+input_provinsi()
+input_kabupaten()
+input_kecamatan()
+
+
+print("")
+print("----------------------")
+print("DATA DIRI ANDA")
+print("Nama anda adalah", nama, ". Anda bertempat tinggal di",
+      daftar_provinsi[pilih_prov-1], ",", kabupaten_anda[pilih_kabupaten-1], ",", kecamatan_anda[pilih_kecamatan-1])
+print("")
